3791c782595807ae7633d7e14db2e738e112419b
sarthakpatel <sr555161@dal.ca>
2023-09-04 22:56:29 -0300

    Observables added

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -15,6 +15,8 @@ export class TasksComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks()
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
   }
 }
--- angular-crash/src/app/services/task.service.ts
+++ angular-crash/src/app/services/task.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import {Task} from "../Task";
-import {TASKS} from "../mock-tasks"
+import {TASKS} from "../mock-tasks";
+import { Observable, of } from 'rxjs';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getTasks(): Task[] {
-    return TASKS
+  getTasks(): Observable<Task[]> {
+    //const tasks = of(TASKS);
+    //return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
